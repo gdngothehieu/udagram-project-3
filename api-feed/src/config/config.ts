1839c4dfaf4c8694607f14f9ebc4f437eb8d3c71
gdngothehieu <cusaniv@gmail.com>
2022-04-30 19:24:47 -0700

    Update

--- api-feed/src/config/config.ts
+++ api-feed/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": process.env.POSTGRESS_USERNAME,
-    "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DB,
-    "host": process.env.POSTGRESS_HOST,
+    "username": process.env.POSTGRESS_USERNAME || "postgres",
+    "password": process.env.POSTGRESS_PASSWORD || "Aa12345678!",
+    "database": process.env.POSTGRESS_DB || "postgres",
+    "host": process.env.POSTGRESS_HOST || "aws-first-postgres.cjsdgvpdyjwe.ap-southeast-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_reigion": process.env.AWS_REGION,
-    "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET,
+    "aws_reigion": process.env.AWS_REGION || "ap-southeast-1",
+    "aws_profile": process.env.AWS_PROFILE ,
+    "aws_media_bucket": process.env.AWS_BUCKET || "arn:aws:s3:::my-aws-bucket-primary-one",
     "url": process.env.URL    
   },
   "prod": {
