c00435048ce708c830583cc4c7191b1360605612
gdngothehieu <cusaniv@gmail.com>
2022-05-01 17:19:19 -0700

    Update

--- deployment/k8s/deploy_fluentd.sh
+++ deployment/k8s/deploy_fluentd.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-microk8s kubectl apply -f cloudwatch-namespace.yaml
+ kubectl apply -f cloudwatch-namespace.yaml
 
-microk8s kubectl create configmap cluster-info \
+ kubectl create configmap cluster-info \
 --from-literal=cluster.name=udagram \
 --from-literal=logs.region=us-west-2 -n amazon-cloudwatch
 
-microk8s kubectl apply -f fluentd.yml
+ kubectl apply -f fluentd.yml
