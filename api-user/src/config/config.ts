9c59a2caf54e71d1b64100d7f627835736d89ddd
gdngothehieu <cusaniv@gmail.com>
2022-05-02 19:51:02 -0700

    Update

--- api-user/src/config/config.ts
+++ api-user/src/config/config.ts
@@ -8,13 +8,13 @@ export const config = {
     "aws_reigion": process.env.AWS_REGION || "ap-southeast-1",
     "aws_profile": process.env.AWS_PROFILE ,
     "aws_media_bucket": process.env.AWS_BUCKET || "arn:aws:s3:::my-aws-bucket-primary-one",
-    "url": process.env.URL    
+    "url": process.env.URL    || "http://localhost:4200/"
   },
   "prod": {
-    "username": "postgres",
-    "password": "Aa12345678!",
-    "database": "udagram_prod",
-    "host": "",
+    "username": process.env.POSTGRESS_USERNAME || "postgres",
+    "password": process.env.POSTGRESS_PASSWORD || "Aa12345678!",
+    "database": process.env.POSTGRESS_DB ||  "postgres",
+    "host": process.env.POSTGRESS_HOST || "aws-first-postgres.cjsdgvpdyjwe.ap-southeast-1.rds.amazonaws.com",
     "dialect": "postgres"
   },
   "jwt": {
@@ -22,3 +22,5 @@ export const config = {
   }
 
 }
+// AKIASATJDB7HYMPTR2KX
+// 8lq3rIRzfkl9NQaaSnal/aQSK120TFpvjhE/Pvcj
